f0488fdf7ca13db2a384d5e021131bee89d278c3
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-12-20 12:55:37 -0600

    Mobile header completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,46 @@
 import React, { useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import { Styles } from './Header.styles';
 import { planetColor } from '../../tokens.styles';
 import BurgerIcon from '../../assets/icon-hamburger.svg';
 
 const Header = () => {
 	const { planetName, dataType } = useParams();
-	const [plegableOpened, setPlegableOpened] = useState(false);
+	const navigate = useNavigate();
+	const [plegableOpened, setPlegableOpened] = useState(true);
+	const changePlanet = planet => {
+		navigate(`/planets/${planet}/${dataType}`);
+		setPlegableOpened(false);
+	};
 	return (
 		<Styles accent={planetColor[planetName]}>
-			<section className="header__name">
+			<section className={`header__name ${plegableOpened && 'opened'}`}>
 				<h1>THE PLANETS</h1>
-				<img src={BurgerIcon} alt="" />
+				<img
+					onClick={() => setPlegableOpened(!plegableOpened)}
+					src={BurgerIcon}
+					alt=""
+				/>
 			</section>
 			<section className="header__themes">
-				<Link to={`/planets/${planetName}/overview`}>Overview</Link>
-				<Link to={`/planets/${planetName}/structure`}>Structure</Link>
-				<Link to={`/planets/${planetName}/geology`}>Geology</Link>
+				<Link
+					className={`${dataType === 'overview' && 'active'}`}
+					to={`/planets/${planetName}/overview`}
+				>
+					Overview
+				</Link>
+				<Link
+					className={`${dataType === 'structure' && 'active'}`}
+					to={`/planets/${planetName}/structure`}
+				>
+					Structure
+				</Link>
+				<Link
+					className={`${dataType === 'geology' && 'active'}`}
+					to={`/planets/${planetName}/geology`}
+				>
+					Geology
+				</Link>
 			</section>
 			<section className="header__planets">
 				<Link to={`/planets/mercury/${dataType}`}>Mercury</Link>
@@ -29,37 +53,61 @@ const Header = () => {
 				<Link to={`/planets/neptune/${dataType}`}>Neptune</Link>
 			</section>
 			<section className={`header__plegable ${plegableOpened && 'opened'}`}>
-				<div className="header__plegable__item">
+				<div
+					className="header__plegable__item"
+					onClick={() => changePlanet('mercury')}
+				>
 					<div style={{ backgroundColor: planetColor.mercury }}></div>
-					<Link to={`/planets/mercury/${dataType}`}>Mercury</Link>
+					<p>Mercury</p>
 				</div>
-				<div className="header__plegable__item">
+				<div
+					className="header__plegable__item"
+					onClick={() => changePlanet('venus')}
+				>
 					<div style={{ backgroundColor: planetColor.venus }}></div>
-					<Link to={`/planets/venus/${dataType}`}>Venus</Link>
+					<p>Venus</p>
 				</div>
-				<div className="header__plegable__item">
+				<div
+					className="header__plegable__item"
+					onClick={() => changePlanet('earth')}
+				>
 					<div style={{ backgroundColor: planetColor.earth }}></div>
-					<Link to={`/planets/earth/${dataType}`}>Earth</Link>
+					<p>Earth</p>
 				</div>
-				<div className="header__plegable__item">
+				<div
+					className="header__plegable__item"
+					onClick={() => changePlanet('mars')}
+				>
 					<div style={{ backgroundColor: planetColor.mars }}></div>
-					<Link to={`/planets/mars/${dataType}`}>Mars</Link>
+					<p>Mars</p>
 				</div>
-				<div className="header__plegable__item">
+				<div
+					className="header__plegable__item"
+					onClick={() => changePlanet('jupiter')}
+				>
 					<div style={{ backgroundColor: planetColor.jupiter }}></div>
-					<Link to={`/planets/jupiter/${dataType}`}>Jupiter</Link>
+					<p>Jupiter</p>
 				</div>
-				<div className="header__plegable__item">
+				<div
+					className="header__plegable__item"
+					onClick={() => changePlanet('saturn')}
+				>
 					<div style={{ backgroundColor: planetColor.saturn }}></div>
-					<Link to={`/planets/saturn/${dataType}`}>Saturn</Link>
+					<p>Saturn</p>
 				</div>
-				<div className="header__plegable__item">
+				<div
+					className="header__plegable__item"
+					onClick={() => changePlanet('uranus')}
+				>
 					<div style={{ backgroundColor: planetColor.uranus }}></div>
-					<Link to={`/planets/uranus/${dataType}`}>Uranus</Link>
+					<p>Uranus</p>
 				</div>
-				<div className="header__plegable__item">
+				<div
+					className="header__plegable__item"
+					onClick={() => changePlanet('neptune')}
+				>
 					<div style={{ backgroundColor: planetColor.neptune }}></div>
-					<Link to={`/planets/neptune/${dataType}`}>Neptune</Link>
+					<p>Neptune</p>
 				</div>
 			</section>
 		</Styles>
--- src/Components/Header/Header.styles.js
+++ src/Components/Header/Header.styles.js
@@ -1,5 +1,72 @@
 import styled from 'styled-components';
+import { colorSystem } from '../../tokens.styles';
 
 export const Styles = styled.div`
-	border-bottom: 2px solid ${props => props.accent};
+	.header {
+		&__name {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			padding: 20px 20px;
+			border-bottom: 1px solid ${colorSystem.basePrimaryDesaturated};
+			& > img {
+				cursor: pointer;
+			}
+			&.opened {
+				position: fixed;
+				top: 0;
+				right: 0;
+				left: 0;
+				background-color: ${colorSystem.basePrimary};
+			}
+		}
+		&__plegable {
+			display: none;
+			&.opened {
+				display: block;
+				position: fixed;
+				background-color: ${colorSystem.basePrimary};
+				width: 100%;
+				height: 100vh;
+				top: 77px;
+			}
+			&__item {
+				display: flex;
+				align-items: center;
+				height: 60px;
+				padding: 0 20px;
+				border-bottom: 1px solid ${colorSystem.basePrimaryDesaturated};
+				& div {
+					width: 20px;
+					height: 20px;
+					border-radius: 50%;
+					margin-right: 20px;
+				}
+			}
+		}
+		&__themes {
+			display: grid;
+			grid-template-columns: 1fr 1fr 1fr;
+			border-bottom: 1px solid ${colorSystem.basePrimaryDesaturated};
+			& a {
+				height: 50px;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				font-weight: bold;
+				&.active {
+					border-bottom: 3px solid ${props => props.accent};
+				}
+			}
+		}
+	}
+	@media screen and (min-width: 768px) {
+		.header {
+			&__name {
+				& > img {
+					display: none;
+				}
+			}
+		}
+	}
 `;
